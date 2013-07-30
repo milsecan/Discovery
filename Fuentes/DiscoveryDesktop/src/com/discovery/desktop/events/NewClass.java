de9d2de87bd3536cf88a338b3ffd367aa54925c0
Emanuel <Emanuel@Emanuel-PC>
2013-07-29 21:10:38 -0500

    Creando paquete de prueba

---
+++ Fuentes/DiscoveryDesktop/src/com/discovery/desktop/events/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.discovery.desktop.events;
+
+/**
+ *
+ * @author Emanuel
+ */
+public class NewClass {
+    
+}
