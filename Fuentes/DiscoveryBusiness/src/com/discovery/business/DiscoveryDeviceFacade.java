618de79ee415aee1af9f215f7e4c022e954e03ad
milsecan <milsecan@gmail.com>
2013-08-14 19:44:06 -0500

    cambios milsecan

--- Fuentes/DiscoveryBusiness/src/com/discovery/business/DiscoveryDeviceFacade.java
+++ Fuentes/DiscoveryBusiness/src/com/discovery/business/DiscoveryDeviceFacade.java
@@ -1,6 +1,6 @@
 /*
  * To change this template, choose Tools | Templates
- * and open the template in the editor.XXXXM
+ * and open the template in the editor.
  */
 package com.discovery.business;
 
