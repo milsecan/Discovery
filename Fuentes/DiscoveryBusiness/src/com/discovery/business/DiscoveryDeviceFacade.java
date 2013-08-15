eb7c85db09b3e5e5091f68bbf6b1679e9460ff46
milsecan <milsecan@gmail.com>
2013-08-14 19:40:31 -0500

    vv

--- Fuentes/DiscoveryBusiness/src/com/discovery/business/DiscoveryDeviceFacade.java
+++ Fuentes/DiscoveryBusiness/src/com/discovery/business/DiscoveryDeviceFacade.java
@@ -1,6 +1,6 @@
 /*
  * To change this template, choose Tools | Templates
- * and open the template in the editor.XXXX
+ * and open the template in the editor.XXXXM
  */
 package com.discovery.business;
 
